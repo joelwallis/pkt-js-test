3f22413b4ec3169b81c1dff9cc31f62ad64bb20e
Joel Wallis Jucá <joelwallis@gmail.com>
2017-03-22 16:06:48 -0300

    Users can add and remove images and texts

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,9 +3,12 @@
   /* global API */
   
   var $canvas = $('.canvas .block')
+  var $addText = $('#addText')
   var $imageList = $('.image ul')
+  var closeButtonTpl = '<span class="glyphicon glyphicon-remove"></span>'
   
   $(function () {
+    $addText.click(addTextToCanvasCallback)
     populateImageList()
     
     /**
@@ -18,6 +21,7 @@
             .map(function (imgUrl) {
               return $(document.createElement('img'))
                 .attr('src', imgUrl)
+                .click(addImageToCanvasCallback)
             })
             .map(function (image) {
               return $(document.createElement('li'))
@@ -27,6 +31,49 @@
               $imageList.append(el)
             })
         })
+    }
+    
+    function addTextToCanvas (text) {
+      var tpl = '<div class="canvas-item">@contents @close-btn</div>'
+        .replace('@contents', '<p>@text</p>')
+        .replace('@close-btn', closeButtonTpl)
+        .replace('@text', text)
+      var el = $(tpl)
+      
+      $('.glyphicon', el).click(function (ev) {
+        $(ev.currentTarget).parent().remove()
+      })
+      
+      el.appendTo($canvas)
+    }
+    
+    function addTextToCanvasCallback (ev) {
+      var text = window.prompt('Enter the text to be added to the canvas:')
+      addTextToCanvas(text)
+    }
+    
+    /**
+     * Adds a given image to the HTML canvas (the `<div>` element)
+     */
+    function addImageToCanvas (imgUrl) {
+      var tpl = '<div class="canvas-item">@img @close-btn</div>'
+        .replace('@img', '<img src="@img-url">')
+        .replace('@close-btn', closeButtonTpl)
+        .replace('@img-url', imgUrl)
+      var el = $(tpl)
+      
+      $('.glyphicon', el).click(function (ev) {
+        $(ev.currentTarget).parent().remove()
+      })
+      
+      return el.appendTo($canvas)
+    }
+    
+    /**
+     * Helper to be used as click callack on image elements of the sidebar
+     */
+    function addImageToCanvasCallback (ev) {
+      return addImageToCanvas($(ev.currentTarget).attr('src'))
     }
   })
 })(jQuery)
